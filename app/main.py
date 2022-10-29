68a9e5a07e5fb511e68c4865dd7fa1edd36039c2
hwc9169 <hwc9169@gmail.com>
2022-10-29 22:56:00 +0900

    오류 수정

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 @app.get("/contour-middleware-test/header")
 async def test_header(request: Request, response: Response, allow_credential: bool = True):
     if allow_credential:
-        response.headers["Access-Control-Allow-Credentials"] = True
+        response.headers["Access-Control-Allow-Credentials"] = "true"
 
     return request.headers
